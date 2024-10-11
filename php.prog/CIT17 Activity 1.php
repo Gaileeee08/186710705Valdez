01539d60d910531ca5153765f3bc82eaeea3098c
ShareCodeApp <Cameronastudillo30@gmail.com>
2024-10-11 19:01:18 +0800

    CIT 17 Activity 1: Number Counter

---
+++ php.prog/CIT17 Activity 1.php
@@ -0,0 +1,16 @@
+<?php
+//initialize a variable to 2
+$i = 2;
+
+// Start the while loop, which will run as long as $i is less than or equal to 20
+while ($i <= 20){
+
+   // Print the current value of $i (even number)
+    echo $i. " ";
+   
+    // Increment $i by 2 in each loop interation to ensure only even numbers are printed
+    $i += 2;
+}
+
+
+?>
